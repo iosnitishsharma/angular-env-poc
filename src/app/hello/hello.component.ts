72917d352a17845f9fb0fac8c3e0d3faa2115d8b
Nitish Sharma <ios.nitishsharma@gmail.com>
2024-09-05 23:37:01 +0530

    setup dev

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { environment } from "../../environments/enviornmnet"
 
 @Component({
   selector: 'app-hello',
-  standalone: true,
-  imports: [],
-  templateUrl: './hello.component.html',
-  styleUrl: './hello.component.css'
+  template: `
+    <div>
+      <h1>Welcome to {{ environmentName }} Environment</h1>
+      <p>API URL: {{ apiUrl }}</p>
+    </div>
+  `
 })
 export class HelloComponent {
-
+  environmentName = environment.environmentName;
+  apiUrl = environment.apiUrl;
 }
