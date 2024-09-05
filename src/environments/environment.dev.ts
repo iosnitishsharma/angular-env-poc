106f1d34993dcef9c4173c2d6539ccceda76fb4a
Nitish Sharma <ios.nitishsharma@gmail.com>
2024-09-05 23:45:05 +0530

    setup

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://api-dev.example.com'
+    apiUrl: 'https://dev.api.example.com',
+    environmentName: 'Development'
   };
   
